17b76d456d87db91a3cb49d191e777104f953239
Hugh <vi3t_hugh@yahoo.com>
2016-10-18 18:05:49 -0400

    Added database integration to keep track of registrations

--- server.js
+++ server.js
@@ -49,11 +49,11 @@ var options = {
 var pgp = require('pg-promise')(options);
 var connectionString;
 
-var developmentMode = true;
+var developmentMode = false;
 
 if(developmentMode) {
 	// Enter postgres credentials
-    connectionString = 'postgres://postgres:tuonghuan@localhost:5432/push-notifications';
+    connectionString = 'postgres://postgres:password@localhost:5432/push-notifications';
 } else {
     connectionString = process.env.DATABASE_URL;
     pgp.pg.defaults.ssl = true;
